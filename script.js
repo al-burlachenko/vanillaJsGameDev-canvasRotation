02cb13e1212043409fad926524ed7c303f49d538
Alexandr <alexander@burlachenko>
2022-11-09 13:34:42 +0200

    adds rotation, falling and correct randomizer choosing pumpkins

--- script.js
+++ script.js
@@ -4,43 +4,59 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const numberOfParticles = 200;
-let particlesArray = [];
+const particlesArray = [];
 const pumpkin = new Image();
 pumpkin.src = "pumpkins.png";
+
+// // pushing entire drawing content to coordinates we passed to it/rotate etc.
+// ctx.translate(100, 100);
+// ctx.rotate((10 * Math.PI) / 360);
+// ctx.fillRect(0, 0, 100, 150);
 
 class Particle {
   constructor() {
     this.x = Math.random() * canvas.width;
     this.y = Math.random() * canvas.height;
-    this.size = Math.random() * 100 + 50;
-    this.speed = Math.random() * 5 + 1;
+    this.size = Math.random() * 70 + 10;
+    this.speed = Math.random() * 2 + 0.5;
     this.angle = Math.random() * 360;
     this.spin = Math.random() < 0.5 ? -1 : 1;
 
-    this.pumpkinWidth = 300;
-    this.pumpkinHeight = 300;
-    this.pumpkinFrame = Math.floor(Math.random() * 3);
+    this.frameX = Math.floor(Math.random() * 3);
+    this.frameY = Math.floor(Math.random() * 3);
+    this.spriteSize = 300;
   }
   draw() {
-    // ctx.fillRect(this.x, this.y, this.size, this.size);
-    // ctx.drawImage(pumpkin, this.x, this.y, this.size, this.size);
+    ctx.save(); // remembers curr state of canvas at the point we call it
+
+    ctx.translate(this.x, this.y);
+    ctx.rotate((this.spin * this.angle * Math.PI) / 360);
+
     ctx.drawImage(
       pumpkin,
-      this.pumpkinWidth * this.pumpkinFrame,
-      this.pumpkinHeight * this.pumpkinFrame,
-      this.pumpkinWidth,
-      this.pumpkinHeight,
-      this.x,
-      this.y,
+      this.spriteSize * this.frameX,
+      this.spriteSize * this.frameY,
+      this.spriteSize,
+      this.spriteSize,
+      // translate resets coordinates so we dont want to double them
+      // this.x,
+      // this.y,
+      0 - this.size / 2,
+      0 - this.size / 2,
+      //  this.size / 2 place center of pumpkin in rotation centerpoint
       this.size,
       this.size
     );
+
+    ctx.restore();
+    // return to prev save point no matter what changes were made in between
   }
   update() {
-    if (this.y > canvas.height) {
+    this.angle += 2;
+    if (this.y - this.size > canvas.height) {
       this.y = 0 - this.size;
       this.x = Math.random() * canvas.width;
-      this.size = Math.random() * 100 + 50;
+      this.size = Math.random() * 70 + 10;
       this.speed = Math.random() * 5 + 1;
       this.pumpkinFrame = Math.floor(Math.random() * 3);
     }
@@ -48,12 +64,19 @@ class Particle {
   }
 }
 
-const particle1 = new Particle();
+function init() {
+  for (let i = 0; i < numberOfParticles; i += 1) {
+    particlesArray.push(new Particle());
+  }
+}
+init();
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  particle1.update();
-  particle1.draw();
+  particlesArray.forEach((particle) => {
+    particle.update();
+    particle.draw();
+  });
   requestAnimationFrame(animate);
 }
 animate();
